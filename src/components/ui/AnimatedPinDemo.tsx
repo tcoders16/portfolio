519d0ba85d522fdec40c4bfe74504fe0690bd83c
tcoders16 <tcoders08164002@gmail.com>
2025-07-16 18:28:48 -0400

    added username

--- src/components/ui/AnimatedPinDemo.tsx
+++ src/components/ui/AnimatedPinDemo.tsx
@@ -7,7 +7,7 @@ export default function AnimatedPinDemo() {
     <div className="h-[40rem] w-full flex items-center justify-center poppins-thin">
       <PinContainer
         title="Thankyou 😇"
-        href="https://twitter.com/mannupaaji"
+        href="https://x.com/Ceo_HSD"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
@@ -31,3 +31,4 @@ export default function AnimatedPinDemo() {
     </div>
   );
 }
+
