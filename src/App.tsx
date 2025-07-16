20f1129d42eee6bb93759a5c0de58e20359c673f
tcoders16 <tcoders08164002@gmail.com>
2025-07-16 19:35:28 -0400

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import {TimelineDemo} from './components/ui/TimelineDemo';
 import { SignupFormDemo } from './components/ui/SignupFormDemo';
 import { AnimatedTestimonialsDemo } from './components/ui/AnimatedTestimonialsDemo';
 import ApplePinDemo from './components/ui/AnimatedPinDemo'
+
+import { FloatingNav } from './components/FloatingNav';
+
 const App = () => {
   return (
     <div>
@@ -16,9 +19,18 @@ const App = () => {
       <ZoomOnScroll />
       {/* Section reveal on scroll */}
       <RevealSection />
+
+  {/* Navigation  */}
+    <div className="fixed left-0 right-0 top-[42rem] z-[999] flex justify-center">
+      <FloatingNav />
+    </div>
+
+
+
       <div>
         <ApplePinDemo/>
       </div>
+
 
       {/* Tech Stack  */}
       <div>
